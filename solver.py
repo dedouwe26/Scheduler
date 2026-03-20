5e88df4b7637f125faa9123c14f1ab6cdefc0e33
0xDED <63008025+dedouwe26@users.noreply.github.com>
2026-03-20 14:20:02 +0100

    Modify strategy

--- solver.py
+++ solver.py
@@ -8,42 +8,55 @@ class Solver:
 		self.activities = activities
 		self.participants = participants
 		self.maxParticipants = math.ceil(len(participants) / len(activities))
-		self.activityCount = [0 for i in range(len(activities))]
+		self.solution = [0 for i in range(len(participants))]
 	
 	def solve(self) -> dict[str, list[str]] | None:
-		if not self.descend():
-			return None
+		self.descend()
+
 		schedule = {a: [] for a in self.activities}
-		for participantIndex in range(len(self.solutionPath)):
+		for participantIndex in range(len(self.solution)):
 			name = list(self.participants)[participantIndex]
 			participant = self.participants[name]
-			activity = participant[self.solutionPath[participantIndex]]
+			activity = participant[self.solution[participantIndex]]
 			schedule[activity].append(name)
 		return schedule
-			
+
 	activityCount: list[int]
-	solutionPath: list[int] = []
-	def descend(self) -> bool:
-		"""
-		Descends in the solution path.
-		Returns true if it has found a valid path, otherwise false.
-		"""
-		currentIndex = len(self.solutionPath)
-		if currentIndex == len(self.participants):
-			return True
+	solution: list[int]
+	def descend(self):
+		self.activityCount = [0 for i in range(len(self.activities))]
+		isValidSolution = True
+		for choice in self.solution:
+			if self.activityCount[choice] >= self.maxParticipants:
+				isValidSolution = False
+				break
+			self.activityCount[choice] += 1
+		
+		if isValidSolution:
+			return
 
-		currentParticpant = self.participants[list(self.participants)[currentIndex]]
-		for choice in range(len(currentParticpant)):
-			activity = currentParticpant[choice]
-			activityIndex = self.activities.index(activity)
-			if self.activityCount[activityIndex]+1 > self.maxParticipants:
-				continue
-			self.activityCount[activityIndex] += 1
-			self.solutionPath.append(choice)
-			if not self.descend():
-				self.activityCount[activityIndex] -= 1
-				self.solutionPath.pop()
-				continue
-			return True
+		max_score = -1
+		index = -1
+		for i in range(len(self.participants)):
+			score = self.rate_lowering(i)
+			if score > max_score:
+				max_score = score
+				index = i
+		
+		self.lower_choice(index)
 
-		return False
+		self.descend()
+
+	def rate_lowering(self, index) -> int:
+		# TODO: Count in maxparticipants and current activity count.
+		choices = self.participants[list(self.participants)[index]]
+		return len(choices) - self.solution[index]
+	def lower_choice(self, index):
+		"""
+		Lowers the choice for 1 participant at the specified index.
+		"""
+		self.solution[index] += 1
+		choices = self.participants[list(self.participants)[index]]
+		if self.solution[index] >= len(choices):
+			# TODO: Choose an activity not on choice list.
+			pass
